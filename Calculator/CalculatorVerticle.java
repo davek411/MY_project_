4179de4cff228a67a5255c3073e65d12f7a6c370
davek411 <141879647+davek411@users.noreply.github.com>
2024-03-14 16:53:22 +0530

    Update CalculatorVerticle.java

--- Calculator/CalculatorVerticle.java
+++ Calculator/CalculatorVerticle.java
@@ -8,8 +8,7 @@ import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 import io.vertx.serviceproxy.ServiceBinder;
-//import tinkering.DatabaseService;
-//import tinkering.DatabaseServiceImpl;
+
 
 public class CalculatorVerticle extends AbstractVerticle {
 
@@ -19,15 +18,9 @@ public class CalculatorVerticle extends AbstractVerticle {
 
   public void start(Promise<Void> startPromise) {
     logger.info("Calculator verticle deployed");
-   // vertx.eventBus().consumer()
-//    vertx.eventBus().consumer("calculate", extractor ->
-//   {
-//
-
 
      MessageConsumer<JsonObject> consumer;// = vertx.eventBus().consumer("serviceAddress");
      calculateService = new CalculateServiceImpl(vertx);
-//    databaseService = DatabaseService.create(vertx);
      consumer = new ServiceBinder(vertx)
        .setAddress("calculateserviceAddress")
        .register(CalculateService.class, calculateService);
@@ -38,9 +31,7 @@ public class CalculatorVerticle extends AbstractVerticle {
     DatabaseService databaseService = DatabaseService.createProxy(vertx, "databaseserviceAddress");
 
 
-//    JsonObject databasepayload = new JsonObject();
-//    databasepayload.put("expression", expression);
-//    databasepayload.put("result", result);
+
 
     vertx.eventBus().consumer("payload for database", message -> {
         JsonObject data = new JsonObject();
@@ -51,70 +42,7 @@ public class CalculatorVerticle extends AbstractVerticle {
       databaseService.receiveandstore(data);
 
     });
-////      logger.debug("inside consume..");
-////      String[] values = ((String) message.body()).split(",");
-////      int x = Integer.parseInt(values[0]);
-////      int y = Integer.parseInt(values[1]);
-////      System.out.println("x: "+x);
-////      System.out.println("y: "+y);
-////      int result = x + y; // Perform calculation (you can modify this to perform any operation)
-////
-////      // Reply back with the result
-////      message.reply(result);
-////    });
 
-
-
-
-
-
-
-
-
-//
-//
-//
-//
-//
-//
-//      JsonObject data = new JsonObject();
-//        data = (JsonObject) extractor.body();
-//
-//      String xValue = data.getString("xvalue");
-//      String yValue = data.getString("yvalue");
-//      String messageString = data.getString("message");
-//
-//
-//      double x = Double.parseDouble(xValue);
-//      double y = Double.parseDouble(yValue);
-//      System.out.println("x: "+ xValue);
-//      System.out.println("y: "+ yValue);
-//      double result = x + y; // Perform calculation (you can modify this to perform any operation)
-//      // Reply back with the result
-//      extractor.reply(result);
-//      String expression = String.format("%.2f + %.2f = %.2f", x, y,x+y);
-//
-////    });
-////    vertx.eventBus().consumer("calculate", message -> {
-////      logger.debug("inside consume..");
-////      String[] values = ((String) message.body()).split(",");
-////      int x = Integer.parseInt(values[0]);
-////      int y = Integer.parseInt(values[1]);
-////      System.out.println("x: "+x);
-////      System.out.println("y: "+y);
-////      int result = x + y; // Perform calculation (you can modify this to perform any operation)
-////
-////      // Reply back with the result
-////      message.reply(result);
-////    });
-//   JsonObject databasepayload = new JsonObject();
-//   databasepayload.put("expression", expression);
-//     databasepayload.put("result", result);
-//
-//
-//     vertx.eventBus().request("databaselinkaddress", databasepayload );
-//
-//    startPromise.complete();
   }
 }
 
